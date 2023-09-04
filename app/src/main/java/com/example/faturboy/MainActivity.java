be41ca3cf2ec6ec55a8840f70fdd377c7c27286f
faturdzaky7124 <faturrahmandzaky2@gmail.com>
2023-09-04 09:58:12 +0700

    second gandi

--- app/src/main/java/com/example/faturboy/MainActivity.java
+++ app/src/main/java/com/example/faturboy/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.faturboy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView txtName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    txtName= findViewById(R.id.txt_name);
+    txtName.setText("Bismillah");
     }
 }
